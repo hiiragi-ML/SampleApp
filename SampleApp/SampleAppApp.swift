5114beb3b7f2e8cf4a10e4f70f78a144ac6d19c6
hiiragi-ML <shuseiito@gmail.com>
2021-12-08 09:08:11 +0900

    Initial Commit

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by 伊藤柊惺 on 2021/12/08.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
